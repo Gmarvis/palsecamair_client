c656e807b19d9999a979850ebbac55c078cc542d
Sam Gmarvis <sgmarvis@gmail.com>
2023-11-09 15:33:59 +0100

    added logo to sidenav

--- components/navBar/SideNav.tsx
+++ components/navBar/SideNav.tsx
@@ -5,6 +5,10 @@ import React from "react";
 import { MdDashboard } from "react-icons/md";
 import { PiArticleMediumFill } from "react-icons/pi";
 import { SlEvent } from "react-icons/sl";
+import { CgProfile } from "react-icons/cg";
+
+import Tippy from "@tippy.js/react";
+import "tippy.js/dist/tippy.css";
 
 const links = [
   {
@@ -25,17 +29,7 @@ const links = [
   {
     pathName: "Dashboard",
     url: "/",
-    icon: <MdDashboard />,
-  },
-  {
-    pathName: "Dashboard",
-    url: "/",
-    icon: <MdDashboard />,
-  },
-  {
-    pathName: "Dashboard",
-    url: "/",
-    icon: <MdDashboard />,
+    icon: <CgProfile />,
   },
 ];
 
@@ -43,26 +37,31 @@ export const SideNav = () => {
   return (
     <div className="absolute">
       <div className="flex flex-col bg-gray-400 h-[100vh] gap-20 absolute">
-      <Image 
-      src="/newslogochanged.png"
-      alt="pulse camair logo"
-      width={30}
-      height={30}
-      className="self-center mt-5"
-      />
+        <Image
+          src="/newslogochanged.png"
+          alt="pulse camair logo"
+          width={30}
+          height={30}
+          className="self-center mt-5"
+        />
         <div className="flex flex-col gap-5">
           {links.map((link, index) => (
-            <Link
-              href={link.url}
-              key={index}
-              className={`flex items-center text-dark gap-2 hover:bg-green-600 p-4`}
-            >
-              <p className="text-3xl">{link.icon}</p>
-              {/* <p>{link.pathName}</p> */}
-            </Link>
+            <Tippy content={link.pathName} key={index} placement="right">
+              <Link
+                href={link.url}
+                key={index}
+                className={`flex items-center text-dark gap-2 hover:bg-green-600 p-4`}
+              >
+                <p className="text-3xl">{link.icon}</p>
+                {/* <p>{link.pathName}</p> */}
+              </Link>
+            </Tippy>
           ))}
         </div>
       </div>
+      <button>
+        logout
+      </button>
     </div>
   );
 };
