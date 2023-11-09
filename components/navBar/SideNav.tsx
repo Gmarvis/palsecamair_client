9fda2466d472ede1b77670648a6aba73d094d777
Sam Gmarvis <sgmarvis@gmail.com>
2023-11-09 17:24:11 +0100

    updated the dashboard layout

--- components/navBar/SideNav.tsx
+++ components/navBar/SideNav.tsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { MdDashboard } from "react-icons/md";
-import { PiArticleMediumFill } from "react-icons/pi";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { PiArticleMediumLight } from "react-icons/pi";
 import { SlEvent } from "react-icons/sl";
 import { CgProfile } from "react-icons/cg";
 
@@ -14,12 +14,12 @@ const links = [
   {
     pathName: "Dashboard",
     url: "/",
-    icon: <MdDashboard />,
+    icon: <MdOutlineDashboardCustomize />,
   },
   {
     pathName: "Articles",
     url: "/",
-    icon: <PiArticleMediumFill />,
+    icon: <PiArticleMediumLight />,
   },
   {
     pathName: "Events",
@@ -35,7 +35,7 @@ const links = [
 
 export const SideNav = () => {
   return (
-      <div className="flex flex-col h-[100vh] bg-slate-200 w-fit gap-20">
+      <div className="flex flex-col h-[100vh] bg-slate-200 w-fit gap-20 pt-5">
       
         <div className="flex flex-col gap-5">
           {links.map((link, index) => (
@@ -43,7 +43,7 @@ export const SideNav = () => {
               <Link
                 href={link.url}
                 key={index}
-                className={`flex items-center text-dark hover:text-themecolor gap-2 p-4`}
+                className={`flex items-center text-black font-light hover:text-themecolor gap-2 p-4`}
               >
                 <p className="text-3xl">{link.icon}</p>
                 {/* <p>{link.pathName}</p> */}
